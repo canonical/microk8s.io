832929bda061054cd2289b21d3ba3cb93fae66ac
Scott Mason Nash <sowasred2012@gmail.com>
2019-11-14 16:55:45 +0000

    update install journey

---
+++ js/tabs.js
@@ -0,0 +1,38 @@
+var tabs = document.querySelectorAll('.js-tab');
+
+function setupTabs() {
+  for (var i = 0; i < tabs.length; i += 1) {
+    var tab = tabs[i],
+        tabSelector = tab.getAttribute('href'),
+        tabContent = document.querySelector(tabSelector);
+
+    if (tabContent) {
+      tabContent.classList.add('u-hide');
+      
+      tab.addEventListener('click', function(e) {
+        e.preventDefault();
+        toggleTab(event.currentTarget);
+      });
+    }
+  }
+}
+
+function toggleTab(tab) {
+  var tabSelector = tab.getAttribute('href'),
+      tabContent = document.querySelector(tabSelector);
+
+  tab.classList.toggle('active');
+  tabContent.classList.toggle('u-hide');
+
+  for (var i = 0; i < tabs.length; i += 1) {
+    if (tabs[i] !== tab) {
+      var otherTabSelector = tabs[i].getAttribute('href'),
+          otherTabContent = document.querySelector(otherTabSelector);
+      
+      tabs[i].classList.remove('active');
+      otherTabContent.classList.add('u-hide');
+    }
+  }
+}
+
+setupTabs();
