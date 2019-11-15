05f93dc260204257f05d380b8873fe18a3b4ccad
Scott Mason Nash <sowasred2012@gmail.com>
2019-11-15 10:50:05 +0000

    scroll instructions into view when selecting an OS

--- js/tabs.js
+++ js/tabs.js
@@ -7,8 +7,9 @@ function setupTabs() {
         tabContent = document.querySelector(tabSelector);
 
     if (tabContent) {
+      tab.setAttribute('aria-selected', 'false');
       tabContent.classList.add('u-hide');
-      
+
       tab.addEventListener('click', function(e) {
         e.preventDefault();
         toggleTab(event.currentTarget);
@@ -21,18 +22,21 @@ function toggleTab(tab) {
   var tabSelector = tab.getAttribute('href'),
       tabContent = document.querySelector(tabSelector);
 
-  tab.classList.toggle('active');
-  tabContent.classList.toggle('u-hide');
-
   for (var i = 0; i < tabs.length; i += 1) {
     if (tabs[i] !== tab) {
       var otherTabSelector = tabs[i].getAttribute('href'),
           otherTabContent = document.querySelector(otherTabSelector);
       
+      tabs[i].setAttribute('aria-selected', 'false');
       tabs[i].classList.remove('active');
       otherTabContent.classList.add('u-hide');
     }
   }
+
+  tab.setAttribute('aria-selected', 'true');
+  tab.classList.toggle('active');
+  tabContent.classList.toggle('u-hide');
+  tabContent.scrollIntoView({ behavior: 'smooth' });
 }
 
 setupTabs();
