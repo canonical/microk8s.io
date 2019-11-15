0dd9e99372a828c50cce8e402b14da8efb841196
Scott Mason Nash <sowasred2012@gmail.com>
2019-11-15 15:19:34 +0000

    address js feedback

--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,10 @@
-var tabs = document.querySelectorAll('.js-tab');
-
 function setupTabs() {
+  var tabs = document.querySelectorAll('.js-tab');
+
   for (var i = 0; i < tabs.length; i += 1) {
-    var tab = tabs[i],
-        tabSelector = tab.getAttribute('href'),
-        tabContent = document.querySelector(tabSelector);
+    var tab = tabs[i];
+    var tabSelector = tab.getAttribute('href');
+    var tabContent = document.querySelector(tabSelector);
 
     if (tabContent) {
       tab.setAttribute('aria-selected', 'false');
@@ -12,44 +12,44 @@ function setupTabs() {
 
       tab.addEventListener('click', function(e) {
         e.preventDefault();
-        toggleTab(event.currentTarget);
+        toggleTab(e.currentTarget);
       });
     }
   }
-}
 
-function toggleTab(tab) {
-  var tabSelector = tab.getAttribute('href'),
-      tabContent = document.querySelector(tabSelector);
-
-  for (var i = 0; i < tabs.length; i += 1) {
-    if (tabs[i] !== tab) {
-      var otherTabSelector = tabs[i].getAttribute('href'),
-          otherTabContent = document.querySelector(otherTabSelector);
-      
-      tabs[i].setAttribute('aria-selected', 'false');
-      tabs[i].classList.remove('active');
-      otherTabContent.classList.add('u-hide');
+  function toggleTab(tab) {
+    var tabSelector = tab.getAttribute('href');
+    var tabContent = document.querySelector(tabSelector);
+  
+    for (var i = 0; i < tabs.length; i += 1) {
+      if (tabs[i] !== tab) {
+        var otherTabSelector = tabs[i].getAttribute('href');
+        var otherTabContent = document.querySelector(otherTabSelector);
+        
+        tabs[i].setAttribute('aria-selected', 'false');
+        tabs[i].classList.remove('active');
+        otherTabContent.classList.add('u-hide');
+      }
     }
-  }
-
-  tab.setAttribute('aria-selected', 'true');
-  tab.classList.toggle('active');
-  tabContent.classList.toggle('u-hide');
-
-  if (tab.classList.contains('active')) {
-    var breakpoint = 620,
-        rect = tabContent.getBoundingClientRect(),
-        yOffset = tab.offsetHeight;
-
-    if (window.innerWidth <= breakpoint) {
-      yOffset = 10;
+  
+    tab.setAttribute('aria-selected', 'true');
+    tab.classList.toggle('active');
+    tabContent.classList.toggle('u-hide');
+  
+    if (tab.classList.contains('active')) {
+      var breakpoint = 620;
+      var rect = tabContent.getBoundingClientRect();
+      var yOffset = tab.offsetHeight;
+  
+      if (window.innerWidth <= breakpoint) {
+        yOffset = 10;
+      }
+  
+      window.scroll({
+        behavior: 'smooth',
+        top: (pageYOffset + rect.top) - yOffset
+      });
     }
-
-    window.scroll({
-      behavior: 'smooth',
-      top: (pageYOffset + rect.top) - yOffset
-    });
   }
 }
 
