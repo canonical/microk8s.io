5b6c95a374b6c57bee082e7982a017805133c5c3
Thomas Bille <toto@canonical.com>
2020-04-24 13:51:30 +0100

    Add docs

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,10 @@
 from canonicalwebteam.flask_base.app import FlaskBase
+from canonicalwebteam.discourse_docs import (
+    DiscourseDocs,
+    DiscourseAPI,
+    DocParser,
+)
+
 from flask import render_template
 
 # Rename your project below
@@ -15,3 +21,19 @@ app = FlaskBase(
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+url_prefix = "/docs"
+server_docs_parser = DocParser(
+    api=DiscourseAPI(base_url="https://discourse.ubuntu.com/"),
+    category_id=41,
+    index_topic_id=15536,
+    url_prefix=url_prefix,
+)
+server_docs = DiscourseDocs(
+    parser=server_docs_parser,
+    document_template="/docs/document.html",
+    url_prefix=url_prefix,
+)
+
+server_docs.init_app(app)
