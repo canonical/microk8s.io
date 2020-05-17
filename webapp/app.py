0fee693b72c23d280caf71677974d9b0ccc619ce
Anthony Dillon <me@anthonydillon.com>
2020-05-17 17:58:06 +0100

    Add discourse docs module

--- webapp/app.py
+++ webapp/app.py
@@ -2,6 +2,12 @@
 from canonicalwebteam.flask_base.app import FlaskBase
 from flask import render_template
 
+from canonicalwebteam.discourse_docs import (
+    DiscourseDocs,
+    DocParser,
+    DiscourseAPI,
+)
+
 # Rename your project below
 app = FlaskBase(
     __name__,
@@ -12,22 +18,28 @@ app = FlaskBase(
     template_500="500.html",
 )
 
+from canonicalwebteam.discourse_docs import (
+    DiscourseDocs,
+    DocParser,
+    DiscourseAPI,
+)
+
+doc_parser = DocParser(
+    api=DiscourseAPI(base_url="https://discourse.ubuntu.com/"),
+    index_topic_id=15536,
+    category_id=41,
+    url_prefix="/docs",
+)
+if app.debug:
+    doc_parser.api.session.adapters["https://"].timeout = 99
+discourse_docs = DiscourseDocs(
+    parser=doc_parser,
+    document_template="docs/document.html",
+    url_prefix="/docs",
+)
+discourse_docs.init_app(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/docs")
-def docs():
-    return render_template("docs/index.html")
-
-
-@app.route("/docs/faq")
-def faq():
-    return render_template("docs/faq.html")
-
-
-@app.route("/docs/protocol")
-def protocol():
-    return render_template("docs/protocol.html")
